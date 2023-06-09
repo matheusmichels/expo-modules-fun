fb760ea7fe09dd5100d77feb1275d43e5ae2fc8d
matheusmichels <matheusmichels@outlook.com.br>
2023-06-09 16:31:15 -0300

    Add element transition in between screens

--- src/screens/CarDetails.tsx
+++ src/screens/CarDetails.tsx
@@ -1,11 +1,18 @@
 import { useNavigation } from "@react-navigation/native";
 import { Button, View } from "react-native";
+import Animated from "react-native-reanimated";
+import { sharedElementTransition } from "../animations/sharedElementTransition";
 
 export function CarDetails() {
   const navigation = useNavigation<any>();
 
   return (
-    <View style={{ flex: 1, marginTop: 50 }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Animated.View
+        style={{ width: 100, height: 100, backgroundColor: "green" }}
+        sharedTransitionTag="sharedTag"
+        sharedTransitionStyle={sharedElementTransition}
+      />
       <Button title="Go back" onPress={() => navigation.navigate("Home")} />
     </View>
   );
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,20 @@
 import { useNavigation } from "@react-navigation/native";
 import { Button, View } from "react-native";
+import Animated from "react-native-reanimated";
 
 import * as MyModule from "../../modules/my-module";
+import { sharedElementTransition } from "../animations/sharedElementTransition";
 
 export function Home() {
   const navigation = useNavigation<any>();
 
   return (
     <View style={{ flex: 1 }}>
+      <Animated.View
+        style={{ width: 150, height: 150, backgroundColor: "green" }}
+        sharedTransitionTag="sharedTag"
+        sharedTransitionStyle={sharedElementTransition}
+      />
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate("CarDetails")}
