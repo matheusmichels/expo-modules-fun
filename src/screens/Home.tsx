6ae6594be7717e5282e0cf5f01161f9d7138ea3b
matheusmichels <matheusmichels@outlook.com.br>
2023-06-09 15:21:35 -0300

    Remove Reanimated from screens (broken)

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
 import { Button, View } from "react-native";
-import Animated from "react-native-reanimated";
 
 import * as MyModule from "../../modules/my-module";
 
@@ -9,10 +8,6 @@ export function Home() {
 
   return (
     <View style={{ flex: 1 }}>
-      <Animated.View
-        style={{ width: 150, height: 150, backgroundColor: "green" }}
-        sharedTransitionTag="sharedTag"
-      />
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate("CarDetails")}
